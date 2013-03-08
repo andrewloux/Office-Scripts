25fb31e5bc19308bd123260830213401b91d70d8
Minto Joseph <mintojoseph at gmail.com>
2013-03-08 17:18:48 +0530

    new file:   QuickClean1.py

---
+++ QuickClean1.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+# QuickClean.py
+# Version 0.04
+
+"""
+Quickly clean music, videos, images and documents into a different directory.
+"""
+
+import argparse
+import glob
+import os
+import shutil
+
+
+parser = argparse.ArgumentParser(description='A quick way to clean out your \
+                                              cluttered folders.')
+parser.add_argument('-s',
+                    '--source',
+                    help='Directory you want to clean out. If no other \
+                          argumnets are provided, will create relevant \
+                          directories under source directory',
+                    required=True)
+parser.add_argument('-m',
+                    '--music',
+                    help='Directory you want to copy music files to. If \
+                          absolute path is not provided, will create \
+                          directory inside the source directory',
+                    default='music',
+                    required=False)
+parser.add_argument('-p',
+                    '--pictures',
+                    help='Directory you want to copy picture files to. If \
+                          absolute path is not provided, will create \
+                          directory inside the source directory',
+                    default='pictures',
+                    required=False)
+parser.add_argument('-v',
+                    '--videos',
+                    help='Directory you want to copy video files to. If \
+                          absolute path is not provided, will create \
+                          directory inside the source directory',
+                    default='videos',
+                    required=False)
+parser.add_argument('-d',
+                    '--documents',
+                    help='Directory you want to copy document files to. If \
+                          absolute path is not provided, will create \
+                          directory inside the source directory',
+                    default='documents',
+                    required=False)
+
+
+args = vars(parser.parse_args())
+source_dir=args.itervalues().next()
+os.chdir(source_dir)
+
+file_types = {
+    "music": ["mp3", "flac", "aac"],
+    "pictures": ["png", "jpg", "bmp", "gif"],
+    "videos": ["avi", "mp4", "flv", "mkv", "mov"],
+    "documents": ["pdf", "xls", "xlsx", "pptx", "docx", "m", "ppt", "doc"],
+}
+
+
+all_files = glob.glob("*")
+
+for label, types in file_types.iteritems():
+    for ext in types:
+        for fname in all_files:
+            if fname.endswith("." + ext):
+                try:
+                    for key, value in args.iteritems():
+                        os.mkdir(value)   
+                except OSError:
+                    pass
+                        shutil.copy(fname, value)
+                        os.remove(fname)
